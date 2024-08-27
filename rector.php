790993be514bde7063220a0a87307e7f11b5c4a5
Vladimir Ilimurzin <ilimurzin@ya.ru>
2024-08-27 16:53:51 +0300

    Установил rector

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/install',
+        __DIR__ . '/lang',
+        __DIR__ . '/lib',
+        __DIR__ . '/tools',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
