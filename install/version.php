f8d61738d6ead269e64f443be8314554c80e4f9a
Vladimir Ilimurzin <ilimurzin@ya.ru>
2024-08-29 12:10:55 +0300

    Поддержал Битрикс24

--- install/version.php
+++ install/version.php
@@ -2,5 +2,5 @@
 
 $arModuleVersion = [
     'VERSION' => '1.0.0',
-    'VERSION_DATE' => '2024-08-26 12:30:00',
+    'VERSION_DATE' => '2024-08-29 12:10:00',
 ];
--- tools/redirect.php
+++ tools/redirect.php
@@ -1,5 +1,7 @@
 <?php
 
+const NOT_CHECK_PERMISSIONS = true;
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php';
 
 if (!\CSocServAuthManager::CheckUniqueKey()) {
